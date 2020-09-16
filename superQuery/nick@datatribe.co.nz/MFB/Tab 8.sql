f4a88ce2633f41831d668ea00ba69f369a313b0f
Nick Guebhard <nick@datatribe.co.nz>
2020-09-16 22:22:11 +1200

    Auto-commit: Wed Sep 16 2020 22:22:01 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
+++ superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
@@ -1,7 +1,29 @@
-SELECT 'Ads BBOX' as product, count(distinct date) as dates 
+select ads.product, ads.dates
+from
+(SELECT 'BBOX' as product, count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_bbox_ext`
 where date >= '2020-09-01'
 union all
-SELECT 'Ads MFB FS' as product, count(distinct date) as dates 
+SELECT 'MFB FS' as product, count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_mfb_fs_ext`
 where date >= '2020-09-01'
+union all
+SELECT 'MFB FS' as product, count(distinct date) as dates 
+FROM `d3-projects-271420.MFB_extraction.ads_made_ext`
+where date >= '2020-09-01') as ads
+
+left join
+
+(SELECT 'BBOX' as product, count(distinct ga_date) as dates 
+FROM `d3-projects-271420.MFB_extraction.ga_bbox_ext`
+where ga_date like '202009%'
+union all
+SELECT 'MFB FS' as product, count(distinct ga_date) as dates 
+FROM `d3-projects-271420.MFB_extraction.ga_mfb_fs_ext`
+where ga_date like '202009%'
+union all
+SELECT 'MFB FS' as product, count(distinct ga_date) as dates 
+FROM `d3-projects-271420.MFB_extraction.ga_made_ext`
+where ga_date like '202009%') as ga
+
+on ads.product = ga.product
