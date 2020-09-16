04f6a4d5fba68e6869409e6b03c40c9557658cbb
Nick Guebhard <nick@datatribe.co.nz>
2020-09-16 22:08:50 +1200

    Auto-commit: Wed Sep 16 2020 22:08:42 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
+++ superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
@@ -1,7 +1,7 @@
 SELECT 'Ads BBOX' as product, count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_bbox_ext`
 where date >= '2020-09-01'
-union
+union all
 SELECT 'Ads MFB FS' as product, count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_mfb_fs_ext`
 where date >= '2020-09-01'
