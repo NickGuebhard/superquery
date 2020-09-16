94cec032d7b8c03cd96c42b9abbb3c089a01d797
Nick Guebhard <nick@datatribe.co.nz>
2020-09-17 11:49:15 +1200

    Auto-commit: Thu Sep 17 2020 11:49:07 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
+++ superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
@@ -1,5 +1,5 @@
 SELECT ga_date, count(ga_date)
 FROM `d3-projects-271420.MFB_extraction.ga_bbox_ext`
-where ga_date like '202005%'
+where ga_date like '202006%'
 group by ga_date
 order by ga_date asc
