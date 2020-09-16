a969b84097500e41ffefb74196caabc28880c432
Nick Guebhard <nick@datatribe.co.nz>
2020-09-16 22:49:34 +1200

    Auto-commit: Wed Sep 16 2020 22:49:21 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
+++ superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
@@ -1,4 +1,4 @@
-select coalesce(ads.product, ga.product) prod, ads.dates ads_dates, ga.dates ga_dates
+select ads.product prod, ads.dates ads_dates, ga.dates ga_dates, fb.dates fb_dates
 from
 (SELECT 'BBOX' as product, count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_bbox_ext`
@@ -27,4 +27,21 @@ FROM `d3-projects-271420.MFB_extraction.ga_made_ext`
 where ga_date like '202009%') as ga
 
 on ads.product = ga.product
-group by prod, ads_dates, ga_dates
+
+left JOIN
+
+(SELECT 'BBOX' as product, count(distinct date_start) as dates 
+FROM `d3-projects-271420.MFB_extraction.fb_bbox_ext`
+where date_start >= '2020-09-01'
+union all
+SELECT 'MFB FS' as product, count(distinct date_start) as dates 
+FROM `d3-projects-271420.MFB_extraction.fb_mfb_fs_ext`
+where date_start >= '2020-09-01'
+union all
+SELECT 'MADE' as product, count(distinct date_start) as dates 
+FROM `d3-projects-271420.MFB_extraction.fb_made_ext`
+where date_start >= '2020-09-01') as fb
+
+on ads.product = fb.product
+
+group by prod, ads_dates, ga_dates, fb_dates
