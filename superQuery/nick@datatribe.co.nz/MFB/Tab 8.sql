749c550cff7300effbcc1d731c0223b690bc1704
Nick Guebhard <nick@datatribe.co.nz>
2020-09-16 22:35:45 +1200

    Auto-commit: Wed Sep 16 2020 22:35:36 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
+++ superQuery/nick@datatribe.co.nz/MFB/Tab 8.sql
@@ -8,7 +8,7 @@ SELECT 'MFB FS' as product, count(distinct date) as dates
 FROM `d3-projects-271420.MFB_extraction.ads_mfb_fs_ext`
 where date >= '2020-09-01'
 union all
-SELECT 'MFB FS' as product, count(distinct date) as dates 
+SELECT 'MADE' as product, count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_made_ext`
 where date >= '2020-09-01') as ads
 
@@ -22,7 +22,7 @@ SELECT 'MFB FS' as product, count(distinct ga_date) as dates
 FROM `d3-projects-271420.MFB_extraction.ga_mfb_fs_ext`
 where ga_date like '202009%'
 union all
-SELECT 'MFB FS' as product, count(distinct ga_date) as dates 
+SELECT 'MADE' as product, count(distinct ga_date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ga_made_ext`
 where ga_date like '202009%') as ga
 
