46178bf8db79b674e12b286ccb0d33d3d261e632
Nick Guebhard <nick@datatribe.co.nz>
2020-09-17 15:30:53 +1200

    Auto-commit: Thu Sep 17 2020 15:30:41 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/date count.sql
+++ superQuery/nick@datatribe.co.nz/MFB/date count.sql
@@ -1,4 +1,4 @@
-select ga.product as product, ga.platform as platform, ga.mth as mth, sum(ga.transactions) as transactions, sum(ads.total_cost) as ads_cost, sum(fb.total_cost) as fb_cost
+select ga.product as product, ga.platform as platform, sum(ga.transactions) as transactions, sum(ads.total_cost) as ads_cost, sum(fb.total_cost) as fb_cost
 from
 
 (SELECT 'BBOX' as product, sum(ga_transactions) as transactions, 'Google Analytics' as platform, substr(ga_date, 4, 2) as mth --count(distinct ga_date) as dates 
@@ -55,4 +55,4 @@ group by date_start) as fb
 
 on ga.product = fb.product and ga.platform = fb.platform
 
-group by product, platform, mth
+group by product, platform
