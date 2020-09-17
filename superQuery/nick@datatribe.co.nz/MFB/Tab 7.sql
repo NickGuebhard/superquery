fba32a989e089149ae4ea63345e8b7e495c4ad6b
Nick Guebhard <nick@datatribe.co.nz>
2020-09-17 13:27:14 +1200

    Auto-commit: Thu Sep 17 2020 13:27:06 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/Tab 7.sql
+++ superQuery/nick@datatribe.co.nz/MFB/Tab 7.sql
@@ -1,4 +1,4 @@
-select date_start
-FROM `d3-projects-271420.MFB_extraction.fb_bbox_ext`
-where date_start >= '2020-06-01'
-group by date_start
+select date
+FROM `d3-projects-271420.MFB_extraction.ads_bbox_ext`
+where Date >= '2020-09-01'
+group by Date
