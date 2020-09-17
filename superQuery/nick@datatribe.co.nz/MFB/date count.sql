c97ed919248222167b110bf03652a8a82fd1cd81
Nick Guebhard <nick@datatribe.co.nz>
2020-09-17 15:23:09 +1200

    Auto-commit: Thu Sep 17 2020 15:22:57 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/date count.sql
+++ superQuery/nick@datatribe.co.nz/MFB/date count.sql
@@ -1,4 +1,4 @@
-select ga.product as prod, ga.platform as plat, ga.mth as mth, sum(ga.transactions) as transactions, sum(ads.cost) as ads_cost, sum(fb.cost) as fb_cost
+select ga.product as prod, ga.platform as plat, ga.mth as mth, sum(ga.transactions) as transactions, sum(ads.total_cost) as ads_cost, sum(fb.total_cost) as fb_cost
 from
 
 (SELECT 'BBOX' as product, sum(ga_transactions) as transactions, 'Google Analytics' as platform, substr(ga_date, 4, 2) as mth --count(distinct ga_date) as dates 
@@ -19,17 +19,17 @@ group by ga_date) as ga
 
 left join
 
-(SELECT 'BBOX' as product, sum(cost) as cost, 'Google Ads' as platform, substr(cast(date as string), 5, 2) as mth --count(distinct date) as dates 
+(SELECT 'BBOX' as product, sum(cost) as total_cost, 'Google Ads' as platform, substr(cast(date as string), 5, 2) as mth --count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_bbox_ext`
 where date >= '2020-01-01'
 group by date
 union all
-SELECT 'MFB FS' as product, sum(cost) as cost, 'Google Ads' as platform, substr(cast(date as string), 5, 2) as mth --count(distinct date) as dates 
+SELECT 'MFB FS' as product, sum(cost) as total_cost, 'Google Ads' as platform, substr(cast(date as string), 5, 2) as mth --count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_mfb_fs_ext`
 where date >= '2020-01-01'
 group by date
 union all
-SELECT 'MADE' as product, sum(cost) as cost, 'Google Ads' as platform, substr(cast(date as string), 5, 2) as mth --count(distinct date) as dates 
+SELECT 'MADE' as product, sum(cost) as total_cost, 'Google Ads' as platform, substr(cast(date as string), 5, 2) as mth --count(distinct date) as dates 
 FROM `d3-projects-271420.MFB_extraction.ads_made_ext`
 where date >= '2020-01-01'
 group by date) as ads
@@ -38,17 +38,17 @@ on ga.product = ads.product and ga.mth = ads.mth
 
 left JOIN
 
-(SELECT 'BBOX' as product, sum(spend) as cost, 'Facebook Ads' as platform, substr(cast(date_start as string), 5, 2) as mth --, count(distinct date_start) as dates 
+(SELECT 'BBOX' as product, sum(spend) as total_cost, 'Facebook Ads' as platform, substr(cast(date_start as string), 5, 2) as mth --, count(distinct date_start) as dates 
 FROM `d3-projects-271420.MFB_extraction.fb_bbox_ext`
 where date_start >= '2020-01-01'
 group by date_start
 union all
-SELECT 'MFB FS' as product, sum(spend) as cost, 'Facebook Ads' as platform, substr(cast(date_start as string), 5, 2) as mth--, count(distinct date_start) as dates 
+SELECT 'MFB FS' as product, sum(spend) as total_cost, 'Facebook Ads' as platform, substr(cast(date_start as string), 5, 2) as mth--, count(distinct date_start) as dates 
 FROM `d3-projects-271420.MFB_extraction.fb_mfb_fs_ext`
 where date_start >= '2020-01-01'
 group by date_start
 union all
-SELECT 'MADE' as product, sum(spend) as cost, 'Facebook Ads' as platform, substr(cast(date_start as string), 5, 2) as mth--, count(distinct date_start) as dates 
+SELECT 'MADE' as product, sum(spend) as total_cost, 'Facebook Ads' as platform, substr(cast(date_start as string), 5, 2) as mth--, count(distinct date_start) as dates 
 FROM `d3-projects-271420.MFB_extraction.fb_made_ext`
 where date_start >= '2020-01-01'
 group by date_start) as fb
