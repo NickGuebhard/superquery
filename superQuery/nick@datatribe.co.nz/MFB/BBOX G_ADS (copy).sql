fb038c68779f8fe720130691676a104dd3ee6ee6
Nick Guebhard <nick@datatribe.co.nz>
2020-09-24 12:08:17 +1200

    Auto-commit: Thu Sep 24 2020 12:08:09 GMT+1200 (New Zealand Standard Time)

--- superQuery/nick@datatribe.co.nz/MFB/BBOX G_ADS (copy).sql
+++ superQuery/nick@datatribe.co.nz/MFB/BBOX G_ADS (copy).sql
@@ -1,4 +1,4 @@
-SELECT COUNT(DISTINCT date_start ) as days, EXTRACT(YEAR from Date), date as year
+SELECT COUNT(DISTINCT date_start ) as days, EXTRACT(YEAR from date_start), date_start as year
 FROM `d3-projects-271420.MFB_extraction.fb_bbox_ext`
 where date_start between '2020-09-01' and '2020-09-30'
-group by date
+group by date_start
