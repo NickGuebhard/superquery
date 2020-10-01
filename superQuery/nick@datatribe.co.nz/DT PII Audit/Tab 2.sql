a54c1c3441f3a00b82c8b16a213d2e7a44398de2
Nick Guebhard <nick@datatribe.co.nz>
2020-10-01 16:59:29 +1300

    Auto-commit: Thu Oct 01 2020 16:59:20 GMT+1300 (New Zealand Daylight Time)

--- superQuery/nick@datatribe.co.nz/DT PII Audit/Tab 2.sql
+++ superQuery/nick@datatribe.co.nz/DT PII Audit/Tab 2.sql
@@ -1,4 +1,4 @@
-SELECT parse_date('%Y%m%d',ga_date) as y FROM `dt-pii-audit.client_concatenated.skinny`
+SELECT * FROM `dt-pii-audit.client_concatenated.skinny`
 -- where current_date('Pacific/Auckland') = date(datetime(insertion_date, 'Pacific/Auckland'))
+where current_date('Pacific/Auckland') = date_add(parse_date('%Y%m%d',ga_date), interval 1 day)
 -- where regexp_contains(ga_date, '2020090(7|8|9)')
-group by y
